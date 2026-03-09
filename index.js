970ff7a603a236dbf44d7b1b923692f896fcc4d9
ishakalistair-cell <ishakalistair@gmail.com>
2026-03-08 21:46:20 -0400

    Task 5

--- index.js
+++ index.js
@@ -1,24 +1,18 @@
-console.log('1' == 1); //true
-console.log(1 == true); //true
-console.log("false" == false); // "false" is not false
-console.log("false" == true); // "false" is not true either
-
-if("false")
-   console.log("Hello false!") //but "false" is true enough here
-
-console.log('1' === 1); //false
-console.log(1 === true); //false
-console.log("true" === true); //false
-
-
-for(let i=0; i< 50; i++){
-  if(i %3 === 0 && i%5 === 0){ // is divisible by 3 and 5?
-    console.log('fizzbuzz');
-  }else if(i%3 === 0){ // ok it's not divisible by both how about just 3?
-    console.log('fizz');
-  }else if(i%5 === 0){ //no? what about just 5 then?
-    console.log('buzz');
-  }else{              //so its not divisible by 3 or 5 then
-    console.log(i);
-  }
+function happyPrint(string){
+ console.log("😀: "+string);
 }
+
+function sadPrint(string){
+ console.log("😢: "+string);
+}
+
+//This high-order function adds 2 parameters and passes the answer to the callback
+function add(a, b, callback){
+   let ans = a + b;
+   callback(ans);// call the callback and pass the answer to it
+}
+
+//call add passing the callbacks to it
+
+add(5, 10, happyPrint);
+add(11, 12, sadPrint)
